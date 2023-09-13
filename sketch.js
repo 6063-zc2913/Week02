3ba8f007cc9d9d50e38f91c08897ca5d355daab1
Ziyi Chen <zc2913@nyu.edu>
2023-09-13 16:23:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
+let earWidth = 150;
+let faceWidth = 300;
+
 function setup() {
-    createCanvas(600,600);
-    background(50,11,129);
+    createCanvas(600,800);
+    background(255,153,51);
 }
 
 function draw() {
-    fill("orange");
-    ellipse(200,200,100,100);
-    fill(0,255,0);
-    ellipse(400,200,200,200);
-    stroke("blue");
-    strokeWeight(3);
-    rect(500,200,200,200);
+    stroke("black");
+    fill("black");
+    ellipse(300,400,faceWidth,faceWidth);
+    ellipse(150,350,earWidth,earWidth);
+    ellipse(450,350,earWidth,earWidth);
+    fill("white")
+    triangle(280, 430, 320, 430, 300, 400);
+    ellipse(250,350,50,50);
+    ellipse(350,350,50,50);
+    stroke("white");
+    line(250,450, 350, 450);
 }
