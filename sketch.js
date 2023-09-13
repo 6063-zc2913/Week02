e978bd28d511ca668041fd89041cfbde4ff7c06d
Ziyi Chen <zc2913@nyu.edu>
2023-09-13 15:24:44 -0400

    added two ellipsis and one square

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
 function setup() {
+    createCanvas(600,600);
+    background(50,11,129);
 }
 
 function draw() {
+    fill("orange");
+    ellipse(200,200,100,100);
+    fill(0,255,0);
+    ellipse(400,200,200,200);
+    stroke("blue");
+    strokeWeight(3);
+    rect(500,200,200,200);
 }
